7db4b669722dcc32938765c259855d07037cfa74
vivek333-cloud <Vivekahuja265@gmail.com>
2025-02-20 15:04:16 +0000

    Fixed routing for Vercel

--- main.py
+++ main.py
@@ -102,4 +102,4 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
